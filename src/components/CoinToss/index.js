46c7a5ab8bda50172af3098424e020ac8638e8cf
Shubham <samaroraytb@gmail.com>
2023-03-04 08:16:06 +0000

    React Coin Toss

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -6,23 +6,27 @@ class CoinToss extends Component {
 
   clickTossButton = () => {
     const tossResult = Math.floor(Math.random() * 2)
+    if (tossResult === 0) {
+      this.setState(prevState => ({
+        isHead: true,
+        totalRes: prevState.totalRes + 1,
+        headRes: prevState.headRes + 1,
+      }))
+    } else {
+      this.setState(prevState => ({
+        isHead: false,
+        totalRes: prevState.totalRes + 1,
+        tailRes: prevState.tailRes + 1,
+      }))
+    }
   }
 
   render() {
-    const {displayTossNum} = this.state
-    let imgSrc = ''
-    let headRes = 0
-    let tailRes = 0
-    let totalRes = 0
-    if (displayTossNum === 0) {
-      imgSrc = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
-      headRes += 1
-      totalRes += 1
-    } else {
-      imgSrc = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
-      tailRes += 1
-      totalRes += 1
-    }
+    const {isHead, totalRes, headRes, tailRes} = this.state
+
+    const tossImgSrc = isHead
+      ? 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
 
     return (
       <div className="main-container-bg">
@@ -30,7 +34,7 @@ class CoinToss extends Component {
           <h1 className="coin-toss-game-heading">Coin Toss Game</h1>
           <p className="coin-toss-game-paragraph">Heads (or) Tails</p>
           <div className="head-tail-image">
-            <img className="head-tail-img" src={imgSrc} alt="head" />
+            <img className="head-tail-img" src={tossImgSrc} alt="toss result" />
           </div>
           <button
             onClick={this.clickTossButton}
